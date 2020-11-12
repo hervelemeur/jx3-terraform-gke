3b7877ab974c373d635c4547f2048bf80387db0e
Hervé Le Meur <52448671+hervelemeur@users.noreply.github.com>
2020-11-12 18:24:23 +0100

    fix(terraform): the bot token is a sensitive value
    https://www.terraform.io/docs/cloud/workspaces/variables.html\#sensitive-values

--- variables.tf
+++ variables.tf
@@ -106,4 +106,5 @@ variable "jx_bot_username" {
 variable "jx_bot_token" {
   description = "Bot token used to interact with the Jenkins X cluster git repository"
   type        = string
+  sensitive   = true
 }
